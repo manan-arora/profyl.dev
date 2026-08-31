39c94bf7598f2c581d09c80ba496df1386db8cc3
Manan Arora <mananarora1350@gmail.com>
2026-09-01 02:58:02 +0530

    feat: minor UI bug fix

--- src/components/profyl/ProfylIdentityCard.tsx
+++ src/components/profyl/ProfylIdentityCard.tsx
@@ -40,7 +40,6 @@ const LeetcodeIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    id="leetcode"
     {...props}
   >
     <path
